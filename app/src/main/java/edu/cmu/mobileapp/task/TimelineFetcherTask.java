12bee5576fd0b9fd4361d90aed1d37ff8d58e375
Srikrishnan Suresh <krishna.tanjavur@gmail.com>
2015-07-16 22:16:38 -0400

    Adding the progress bar while loading tweets

--- app/src/main/java/edu/cmu/mobileapp/task/TimelineFetcherTask.java
+++ app/src/main/java/edu/cmu/mobileapp/task/TimelineFetcherTask.java
@@ -1,5 +1,6 @@
 package edu.cmu.mobileapp.task;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class TimelineFetcherTask extends AsyncTask<String, Void, List<twitter4j.
 
     private Activity activity;
     private ListView listView;
+    private ProgressDialog progress;
     public TimelineFetcherTask(Activity activity) {
         this.activity = activity;
     }
@@ -28,11 +30,17 @@ public class TimelineFetcherTask extends AsyncTask<String, Void, List<twitter4j.
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+        progress = new ProgressDialog(activity);
+        progress.setMessage("Loading Tweets...");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+        progress.show();
     }
 
     @Override
     protected void onPostExecute(List<twitter4j.Status> statusList) {
         super.onPostExecute(statusList);
+        progress.hide();
         listView = (ListView) activity.findViewById(R.id.listView);
         listView.setAdapter(new ListItemAdapter(activity, statusList));
         listView.setOnItemClickListener(new TweetItemClickListener(activity, statusList));
